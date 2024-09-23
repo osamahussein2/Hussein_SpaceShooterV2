35471ffd422de09d9a87f32381ea6f94ab307e9f
Osama Hussein <osamahusseinn2@gmail.com>
2024-09-23 18:24:44 -0400

    I'm stuck on the player deceleration when releasing input keys

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -16,9 +16,14 @@ public class Player : MonoBehaviour
 
     private float acceleration;
 
+    public float decelerationTime = 3.0f;
+    private float deceleration;
+
     void Start()
     {
         acceleration = maxSpeed / accelerationTime;
+
+        deceleration = -maxSpeed / decelerationTime;
     }
 
     void Update()
@@ -36,24 +41,42 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             direction += Vector3.left;
+
+            acceleration = maxSpeed / accelerationTime;
         }
 
         else if (Input.GetKey(KeyCode.RightArrow))
         {
             direction += Vector3.right;
+
+            acceleration = maxSpeed / accelerationTime;
         }
 
         else if (Input.GetKey(KeyCode.DownArrow))
         {
             direction += Vector3.down;
+
+            acceleration = maxSpeed / accelerationTime;
         }
 
         else if (Input.GetKey(KeyCode.UpArrow))
         {
             direction += Vector3.up;
+
+            acceleration = maxSpeed / accelerationTime;
         }
 
         direction.Normalize();
+
+        if (direction == Vector3.zero)
+        {
+            acceleration -= maxSpeed / decelerationTime;
+
+            if (acceleration <= 0.0f)
+            {
+                acceleration = 0.0f;
+            }
+        }
 
         playerVelocity += acceleration * Time.deltaTime * direction;
         transform.position += playerVelocity * Time.deltaTime;
